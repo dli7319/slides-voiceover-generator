257df1cceaa35ea24167a4c36cb6492ab4b40606
David Li <dli7319@gmail.com>
2022-12-06 17:16:46 -0500

    Add overlay support

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Convert slides and script into a video with TTS voiceover.<br>
 This is a set of bash scripts to help convert a set of slides into a video with a voiceover.<br>
 The voiceover is generated using [coqui-ai/TTS](https://github.com/coqui-ai/TTS).<br>
-The scripts are designed to be run on a Linux machine with docker and ffmpeg installed.
+The scripts are designed to be run on a Linux machine with docker, an nvidia gpu, and ffmpeg installed.
 
 ## Usage
 
@@ -15,3 +15,12 @@ The scripts are designed to be run on a Linux machine with docker and ffmpeg ins
 4. Run `./initialize_durations` to initialize the durations of each slide.
    - You can tune `durations.txt` to your liking, e.g. for overlay videos.
 5. Run `./generate_voiceover.sh` to generate the voiceover.
+6. Run `./stitch_video.sh` to combine the slides and voiceover into a video.<br>
+   Or use your own video editor software to combine the voiceover into a video.
+
+## Notes
+* You can optionally an `overlays.txt` file to add video overlays to the video.<br>
+  This should be a txt file where each line has the format:<br>
+   `filename;width:height;position_x:position_y;start_overlay_time;end_overlay_time;start_time`<br>
+   For example, to put `overlay.mp4` in the top-left corner from time 0s to 10s:<br>
+   `overlay.mp4;200:-2:0:0;0:10:0`
--- stitch_video.sh
+++ stitch_video.sh
@@ -10,34 +10,73 @@ ffmpeg_call="ffmpeg"
 # Get the number of voice segments
 num_voice_segments=$(ls voice_segments | wc -l)
 # Load durations.txt into an array
-readarray -t durations < durations.txt
+readarray -t durations <durations.txt
 # Escape the colons in the durations
 durations=("${durations[@]//:/\\:}")
+# Load overlays file into an array
+readarray -t overlays <overlays.txt
+# Remove empty lines and comments
+overlays=("${overlays[@]// /}")
+overlays=("${overlays[@]//\#*}")
+# Get the number of overlays
+num_overlays=${#overlays[@]}
 
 # Loop over each voice segment
 for i in $(seq 0 $((num_voice_segments - 1))); do
     # Add the slide (i+1) formatted with 2 digits to the ffmpeg call
-    ffmpeg_call="${ffmpeg_call} -loop 1 -i slides/slide-$(printf "%02d" $((i+1))).png"
+    ffmpeg_call="${ffmpeg_call} -loop 1 -i slides/slide-$(printf "%02d" $((i + 1))).png"
     # Add the voice segment to the ffmpeg call
     ffmpeg_call="${ffmpeg_call} -i voice_segments/${i}.wav"
+done
+
+# For each overlay, add the overlay to the ffmpeg call
+for i in $(seq 0 $((num_overlays - 1))); do
+    # Get the overlay for this slide and split on semicolons
+    IFS=';' read -ra my_overlay <<<"${overlays[$i]}"
+    start_time="${my_overlay[5]}"
+    # If start_time is empty, set it to 0
+    if [ -z "$start_time" ]; then
+        start_time="0"
+    fi
+    # Add the overlay to the ffmpeg call
+    ffmpeg_call="${ffmpeg_call} -ss $start_time -i ${my_overlay[0]}"
 done
 
 # Scale each input to 1080p and trim to 10 seconds
 ffmpeg_call="${ffmpeg_call} -filter_complex \""
 for i in $(seq 0 $((num_voice_segments - 1))); do
-    ffmpeg_call="${ffmpeg_call}[$((2*i))]scale=$output_resolution,trim=duration='${durations[$i]}'[v${i}];"
-    ffmpeg_call="${ffmpeg_call}[$((2*i+1)):a]atrim=duration='${durations[$i]}',apad=whole_dur='${durations[$i]}'[a${i}];"
+    ffmpeg_call="${ffmpeg_call}[$((2 * i))]scale=$output_resolution,trim=duration='${durations[$i]}'[v${i}];"
+    ffmpeg_call="${ffmpeg_call}[$((2 * i + 1)):a]atrim=duration='${durations[$i]}',apad=whole_dur='${durations[$i]}'[a${i}];"
 done
 # Concatenate the segments
 for i in $(seq 0 $((num_voice_segments - 1))); do
     ffmpeg_call="${ffmpeg_call}[v${i}][a${i}]"
 done
-ffmpeg_call="${ffmpeg_call}concat=n=${num_voice_segments}:v=1:a=1[outv][outa]\""
-# Set the map to the output video and audio
-ffmpeg_call="${ffmpeg_call} -map \"[outv]\" -map \"[outa]\""
+
+# If we have overlays, add them to the video
+if [ $num_overlays -gt 0 ]; then
+    ffmpeg_call="${ffmpeg_call}concat=n=${num_voice_segments}:v=1:a=1[outv_0][outa]"
+    # Loop over each overlay
+    for i in $(seq 0 $((num_overlays - 1))); do
+        # Get the overlay for this slide and split on semicolons
+        IFS=';' read -ra my_overlay <<<"${overlays[$i]}"
+        overlay_resolution="${my_overlay[1]}"
+        overlay_position="${my_overlay[2]}"
+        overlay_start="${my_overlay[3]}"
+        overlay_end="${my_overlay[4]}"
+        # Add the overlay to the ffmpeg call
+        ffmpeg_call="${ffmpeg_call};[$((2*num_voice_segments + i))]scale=$overlay_resolution,setpts=PTS-STARTPTS+$overlay_start/TB[overlay_${i}];[outv_$i][overlay_${i}]overlay=${overlay_position}:enable='between(t\,$overlay_start\,$overlay_end)'[outv_$((i+1))]"
+    done
+    # Set the map to the output video and audio
+    ffmpeg_call="${ffmpeg_call}\" -map \"[outv_$num_overlays]\" -map \"[outa]\""
+else
+    ffmpeg_call="${ffmpeg_call}concat=n=${num_voice_segments}:v=1:a=1[outv][outa]\""
+    # Set the map to the output video and audio
+    ffmpeg_call="${ffmpeg_call} -map \"[outv]\" -map \"[outa]\""
+fi
 
 # Limit to 60 seconds
-# ffmpeg_call="${ffmpeg_call} -t 60"
+# ffmpeg_call="${ffmpeg_call} -t 6"
 # Output at 30 fps
 ffmpeg_call="${ffmpeg_call} -r 30"
 
